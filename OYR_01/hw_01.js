0d568425b19966ec0f1fd8282385656d007e1d7c
wogha95 <wogha95@naver.com>
2021-04-20 23:22:41 +0900

    File Numbering

--- OYR_01/hw_01.js
+++ OYR_01/hw_01.js
@@ -1,10 +1,10 @@
 
 let imgArray = new Array();
-imgArray[0] = "hw_1_2.jpg";
-imgArray[1] = "hw_1_3.jpg";
-imgArray[2] = "hw_1_4.jpg";
-imgArray[3] = "hw_1_5.jpg";
-imgArray[4] = "hw_1_6.jpg";
+imgArray[0] = "hw_01_02.jpg";
+imgArray[1] = "hw_01_03.jpg";
+imgArray[2] = "hw_01_04.jpg";
+imgArray[3] = "hw_01_05.jpg";
+imgArray[4] = "hw_01_06.jpg";
 
 let imgCnt = 0;
 
